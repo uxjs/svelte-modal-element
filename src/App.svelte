ad76103aa8e2d58b9f8c36ba69a2658a1660c244
compiledcode <ebw@startmail.com>
2022-09-27 02:23:13 +0000

    incomplete commite, dinner-time, using virtual dev environment need to force commit to save ...

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,21 @@
 <script>
+
+  import { onMount, onDestroy } from "svelte";
   import { fade } from "svelte/transition";
 
   export let id = "";
   export let show = false;
   export let width = "600px";
   export let height = "400px";
+
+  export let hour12card;
+  export let hour24card;
+  export let minutecard;
+  export let minutedisplay;
+  export let hourdisplay;
+  export let hourTextBox;
+  export let minuteTextBox;
+
 
   function close(e) {
     dispatchCloseEvent.call(this, e);
@@ -28,6 +39,436 @@
           c-1.172-1.172-3.07-1.172-4.242,0c-1.172,1.171-1.172,3.071,0,4.242l18.865,18.864L0.879,42.85c-1.172,1.171-1.172,3.071,0,4.242
           C1.465,47.677,2.233,47.97,3,47.97s1.535-0.293,2.121-0.879l18.865-18.864L42.85,47.091c0.586,0.586,1.354,0.879,2.121,0.879
           s1.535-0.293,2.121-0.879c1.172-1.171,1.172-3.071,0-4.242L28.228,23.986z`;
+
+          console.log('------- HERE -------')
+
+          // Time-Picker below
+ 
+  // let hour12card;
+  // let hour24card;
+  // let minutecard;
+  // let minutedisplay;
+  // let hourdisplay;
+  // let hourTextBox;
+  // let minuteTextBox;
+
+  // apply CSS to selected minute
+  //
+  let selected_00m = "";
+  let selected_05m = "";
+  let selected_10m = "";
+  let selected_15m = "";
+  let selected_20m = "";
+  let selected_25m = "";
+  let selected_30m = "";
+  let selected_35m = "";
+  let selected_40m = "";
+  let selected_45m = "";
+  let selected_50m = "";
+  let selected_55m = "";
+
+  // apply CSS to selected 12-hour
+  //
+  let selected_00h = "";
+  let selected_01h = "";
+  let selected_02h = "";
+  let selected_03h = "";
+  let selected_04h = "";
+  let selected_05h = "";
+  let selected_06h = "";
+  let selected_07h = "";
+  let selected_08h = "";
+  let selected_09h = "";
+  let selected_10h = "";
+  let selected_11h = "";
+  let selected_12h = "";
+
+  // apply CSS to selected 24-hour
+  //
+  let selected_13h = "";
+  let selected_14h = "";
+  let selected_15h = "";
+  let selected_16h = "";
+  let selected_17h = "";
+  let selected_18h = "";
+  let selected_19h = "";
+  let selected_20h = "";
+  let selected_21h = "";
+  let selected_22h = "";
+  let selected_23h = "";
+
+  const clearAllSelected = async () => {
+    selected_00m = "";
+    selected_05m = "";
+    selected_10m = "";
+    selected_15m = "";
+    selected_20m = "";
+    selected_25m = "";
+    selected_30m = "";
+    selected_35m = "";
+    selected_40m = "";
+    selected_45m = "";
+    selected_50m = "";
+    selected_55m = "";
+
+    selected_00h = "";
+    selected_01h = "";
+    selected_02h = "";
+    selected_03h = "";
+    selected_04h = "";
+    selected_05h = "";
+    selected_06h = "";
+    selected_07h = "";
+    selected_08h = "";
+    selected_09h = "";
+    selected_10h = "";
+    selected_11h = "";
+    selected_12h = "";
+
+    selected_13h = "";
+    selected_14h = "";
+    selected_15h = "";
+    selected_16h = "";
+    selected_17h = "";
+    selected_18h = "";
+    selected_19h = "";
+    selected_20h = "";
+    selected_21h = "";
+    selected_22h = "";
+    selected_23h = "";
+  };
+
+  const hour24panel = () => {
+    hour12card.hidden = false;
+    hour24card.hidden = false;
+    minutecard.hidden = true;
+
+    hour24card.style.color = "white";
+    minutecard.style.color = "#c79395";
+    hour12card.style.color = "green";
+
+    //Header Display
+    minutedisplay.style.color = "#c79395";
+    hourdisplay.style.color = "white";
+  }; //hour24panel
+
+  const minutepanel = () => {
+    // show Minutes Panel Only
+    hour12card.hidden = true;
+    hour24card.hidden = true;
+    minutecard.hidden = false;
+    minutedisplay.style.color = "white";
+    hourdisplay.style.color = "#c79395";
+  };
+
+  onMount(() => {
+    console.log("onMount");
+    //==TODO:FIX hour24panel(); //initialize the app - show 24-hour panel
+    hourTextBox.value='08'  //default shift start time(s) 08:00AM -- default unless otherwise specified
+  });
+
+  onDestroy(() => {
+    console.log("onDestroy");
+  });
+
+  // ---------------------
+  // MINUTE handlers
+  // ---------------------
+  const minute_00 = async () => {
+    let temp = selected_00m === "" ? "circle-me" : "";
+    await clearAllSelected();
+    minutedisplay.innerHTML = "00";
+    minuteTextBox.value='00';
+    selected_00m = temp;
+  };
+
+  const minute_05 = async () => {
+    let temp = selected_05m === "" ? "circle-me" : "";
+    await clearAllSelected();
+    minutedisplay.innerHTML = "05";
+    minuteTextBox.value='05';
+    selected_05m = temp;
+  };
+
+  const minute_10 = async () => {
+    let temp = selected_10m === "" ? "circle-me" : "";
+    await clearAllSelected();
+    minutedisplay.innerHTML = "10";
+    minuteTextBox.value='10';
+    selected_10m = temp;
+  };
+
+  const minute_15 = async () => {
+    let temp = selected_15m === "" ? "circle-me" : "";
+    await clearAllSelected();
+    minutedisplay.innerHTML = "15";
+    minuteTextBox.value='15';
+    selected_15m = temp;
+  };
+
+  const minute_20 = async () => {
+    let temp = selected_20m === "" ? "circle-me" : "";
+    await clearAllSelected();
+    minutedisplay.innerHTML = "20";
+    minuteTextBox.value='20';
+    selected_20m = temp;
+  };
+
+  const minute_25 = async () => {
+    let temp = selected_25m === "" ? "circle-me" : "";
+    await clearAllSelected();
+    minutedisplay.innerHTML = "25";
+    minuteTextBox.value='25';
+    selected_25m = temp;
+  };
+
+  const minute_30 = async () => {
+    let temp = selected_30m === "" ? "circle-me" : "";
+    await clearAllSelected();
+    minutedisplay.innerHTML = "30";
+    minuteTextBox.value='30';
+    selected_30m = temp;
+  };
+
+  const minute_35 = async () => {
+    let temp = selected_35m === "" ? "circle-me" : "";
+    await clearAllSelected();
+    minutedisplay.innerHTML = "35";
+    minuteTextBox.value='35';
+    selected_35m = temp;
+  };
+
+  const minute_40 = async () => {
+    let temp = selected_40m === "" ? "circle-me" : "";
+    await clearAllSelected();
+    minutedisplay.innerHTML = "40";
+    minuteTextBox.value='40';
+    selected_40m = temp;
+  };
+
+  const minute_45 = async () => {
+    let temp = selected_45m === "" ? "circle-me" : "";
+    await clearAllSelected();
+    minutedisplay.innerHTML = "45";
+    minuteTextBox.value='45';
+    selected_45m = temp;
+  };
+
+  const minute_50 = async () => {
+    let temp = selected_50m === "" ? "circle-me" : "";
+    await clearAllSelected();
+    minutedisplay.innerHTML = "50";
+    minuteTextBox.value='50';
+    selected_50m = temp;
+  };
+
+  const minute_55 = async () => {
+    let temp = selected_55m === "" ? "circle-me" : "";
+    await clearAllSelected();
+    minutedisplay.innerHTML = "55";
+    minuteTextBox.value='55';
+    selected_55m = temp;
+  };
+
+  // ----------------------
+  // HOUR handlers (1 - 24)
+  // ----------------------
+
+  const hour_00 = async () => {
+    let temp = selected_00h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "00";
+    hourTextBox.value='00';
+    selected_00h = temp;
+  };
+
+  const hour_1 = async () => {
+    let temp = selected_01h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "1";
+    hourTextBox.value='1';
+    selected_01h = temp;
+  };
+
+  const hour_2 = async () => {
+    let temp = selected_02h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "2";
+    hourTextBox.value='2';
+    selected_02h = temp;
+  };
+
+
+  const hour_3 = async () => {
+    let temp = selected_03h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "3";
+    hourTextBox.value='3';
+    selected_03h = temp;
+  };
+
+  const hour_4 = async () => {
+    let temp = selected_04h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "4";
+    hourTextBox.value='4';
+    selected_04h = temp;
+  };
+
+  const hour_5 = async () => {
+    let temp = selected_05h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "5";
+    hourTextBox.value='5';
+    selected_05h = temp;
+  };
+
+  const hour_6 = async () => {
+    let temp = selected_06h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "6";
+    hourTextBox.value='6';
+    selected_06h = temp;
+  };
+
+  const hour_7 = async () => {
+    let temp = selected_07h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "7";
+    hourTextBox.value='7';
+    selected_07h = temp;
+  };
+
+  const hour_8 = async () => {
+    let temp = selected_08h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "8";
+    hourTextBox.value='8';
+    selected_08h = temp;
+  };
+
+  const hour_9 = async () => {
+    let temp = selected_09h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "9";
+    hourTextBox.value='9';
+    selected_09h = temp;
+  };
+
+  const hour_10 = async () => {
+    let temp = selected_10h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "10";
+    hourTextBox.value='10';
+    selected_10h = temp;
+  };
+
+  const hour_11 = async () => {
+    let temp = selected_11h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "11";
+    hourTextBox.value='11';
+    selected_11h = temp;
+  };
+
+  const hour_12 = async () => {
+    let temp = selected_12h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "12";
+    hourTextBox.value='12';
+    selected_12h = temp;
+  };
+
+  const hour_13 = async () => {
+    let temp = selected_02h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "13";
+    hourTextBox.value='13';
+    selected_13h = temp;
+  };
+
+  const hour_14 = async () => {
+    let temp = selected_14h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "14";
+    hourTextBox.value='14';
+    selected_14h = temp;
+  };
+
+  const hour_15 = async () => {
+    let temp = selected_15h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "15";
+    hourTextBox.value='15';
+    selected_15h = temp;
+  };
+
+  const hour_16 = async () => {
+    let temp = selected_16h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "16";
+    hourTextBox.value='16';
+    selected_16h = temp;
+  };
+
+  const hour_17 = async () => {
+    let temp = selected_17h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "17";
+    hourTextBox.value='17';
+    selected_17h = temp;
+  };
+
+  const hour_18 = async () => {
+    let temp = selected_18h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "18";
+    hourTextBox.value='18';
+    selected_18h = temp;
+  };
+
+  const hour_19 = async () => {
+    let temp = selected_19h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "19";
+    hourTextBox.value='19';
+    selected_19h = temp;
+  };
+
+  const hour_20 = async () => {
+    let temp = selected_20h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "20";
+    hourTextBox.value='20';
+    selected_20h = temp;
+  };
+
+  const hour_21 = async () => {
+    let temp = selected_21h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "21";
+    hourTextBox.value='21';
+    selected_21h = temp;
+  };
+
+  const hour_22 = async () => {
+    let temp = selected_22h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "22";
+    hourTextBox.value='22';
+    selected_22h = temp;
+  };
+
+  const hour_23 = async () => {
+    let temp = selected_23h === "" ? "circle-me" : "";
+    await clearAllSelected();
+    hourdisplay.innerHTML = "23";
+    hourTextBox.value='23';
+    selected_23h = temp;
+  };
+
+console.log('end of script')
+
 </script>
 
 <style>
